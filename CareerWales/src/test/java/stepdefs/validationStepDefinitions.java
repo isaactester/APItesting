657adf22a821498060f692c9f4f0d982ba705f40
munslowl <1738help>
2018-10-23 12:06:06 +0100

    Updated

--- CareerWales/src/test/java/stepdefs/validationStepDefinitions.java
+++ CareerWales/src/test/java/stepdefs/validationStepDefinitions.java
@@ -1,23 +1,14 @@
 package stepdefs;
 
-import com.google.gson.JsonObject;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import io.restassured.RestAssured;
-import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
-import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 
-
 import static io.restassured.RestAssured.given;
-
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.Matchers.contains;
-import static org.hamcrest.core.IsEqual.equalTo;
-
+import static org.hamcrest.CoreMatchers.hasItems;
 import static services.Endpoints.BASE_URL;
 
 public class validationStepDefinitions {
@@ -33,10 +24,12 @@ public class validationStepDefinitions {
     @When("^a user retrieves detail by id$")
     public void a_user_retrieves_details_by_id() {
         given().when().get(BASE_URL + "v1/programmes/").then()
-                .body("id",equalTo(("[1, 2, 11, 12, 13, 14, 15, 16, 17, 18]")))
+                .body("id", hasItems(1, 2, 11, 12, 13, 14, 15, 16, 17, 18));
+
+
                // .body("name.en", equalTo(("nameEn1"));
                // .id("id", containsString(("")));
-                .body("duration", equalTo("[{cy=Ysbeidiau Heulog..., en=Hello Sunshine...}, {cy=Ysbeidiau Heulog..., en=Hello Sunshine...}, {cy=Mae cymorth ar gael dros gyfnod llawn y rhaglen., en=Support is available for the duration of the programme.}, {en=It depends on the type of support}, {cy=Mae cymorth ar gael dros gyfnod llawn y rhaglen neu nes bydd diweithdra yn dechrau., en=Support is available for the duration of the programme or until employment starts.}, {cy=6 Mis (ac eithrio unrhyw mewn gwaith hyfforddi) a wneir i fyny o 120-140 oriau lleoliadau gwaith a 2-8 wythnos cyflogwr hyfforddiant penodol., en=6 Months (excluding any in-work coaching) made up from 120-140 hours work placements and 2-8 weeks employer specific training }, {cy=Mae cymorth ar gael dros gyfnod llawn y rhaglen., en=Support is available for the duration of the programme.}, {cy=Mae cymorth ar gael dros gyfnod llawn y rhaglen., en=Support is available for the duration of the programme.}, {cy=Cam 1 - amrywiol, Mae Cam 2 yn gwrs wythnos o hyd. , en=Phase 1 - variable, Phase 2 is a week long course. }, {cy=Mae cymorth ar gael dros gyfnod llawn y rhaglen., en=Support is available for the duration of the programme.}]"));
+//                .body("duration", equalTo("[{cy=Ysbeidiau Heulog..., en=Hello Sunshine...}, {cy=Ysbeidiau Heulog..., en=Hello Sunshine...}, {cy=Mae cymorth ar gael dros gyfnod llawn y rhaglen., en=Support is available for the duration of the programme.}, {en=It depends on the type of support}, {cy=Mae cymorth ar gael dros gyfnod llawn y rhaglen neu nes bydd diweithdra yn dechrau., en=Support is available for the duration of the programme or until employment starts.}, {cy=6 Mis (ac eithrio unrhyw mewn gwaith hyfforddi) a wneir i fyny o 120-140 oriau lleoliadau gwaith a 2-8 wythnos cyflogwr hyfforddiant penodol., en=6 Months (excluding any in-work coaching) made up from 120-140 hours work placements and 2-8 weeks employer specific training }, {cy=Mae cymorth ar gael dros gyfnod llawn y rhaglen., en=Support is available for the duration of the programme.}, {cy=Mae cymorth ar gael dros gyfnod llawn y rhaglen., en=Support is available for the duration of the programme.}, {cy=Cam 1 - amrywiol, Mae Cam 2 yn gwrs wythnos o hyd. , en=Phase 1 - variable, Phase 2 is a week long course. }, {cy=Mae cymorth ar gael dros gyfnod llawn y rhaglen., en=Support is available for the duration of the programme.}]"));
 
     }
 
